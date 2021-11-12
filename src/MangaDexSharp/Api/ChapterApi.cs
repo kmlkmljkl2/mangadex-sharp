21082cb4f0f96a06aad68b2a22b3a25418ae1474
kmlkmljkl2 <57689055+kmlkmljkl2@users.noreply.github.com>
2021-11-12 16:59:54 +0100

    Revert "Revert "Revert "merge with main"""
    This reverts commit ea608048d537c0cf9ef2d32db44929b1b0e2f1a6.

--- src/MangaDexSharp/Api/ChapterApi.cs
+++ src/MangaDexSharp/Api/ChapterApi.cs
@@ -97,7 +97,7 @@ namespace MangaDexSharp.Api
         public async Task MarkChapterUnread(Guid chapterId, CancellationToken cancelToken = default)
         {
             await DeleteRequest<MangaDexResponse>(
-                   BaseApiPath + "/" + chapterId + "/read",
+                   BaseApiPath + chapterId + "/read",
                    cancelToken);
         }
     }
--- src/MangaDexSharp/Objects/ChapterReadSession.cs
+++ src/MangaDexSharp/Objects/ChapterReadSession.cs
@@ -73,9 +73,7 @@ namespace MangaDexSharp.Objects
                 && MarkAsReadOnClose
                 && !IsClosed)
             {
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-                Close();
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                Close().Start();
             }
         }
 
