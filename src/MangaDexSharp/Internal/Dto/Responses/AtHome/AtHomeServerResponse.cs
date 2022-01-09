72f54c789e3b54f9235c73422160e59964916ea5
kmlkmljkl2 <57689055+kmlkmljkl2@users.noreply.github.com>
2022-01-09 06:09:51 +0100

    Update AtHomeServerResponse.cs

--- src/MangaDexSharp/Internal/Dto/Responses/AtHome/AtHomeServerResponse.cs
+++ src/MangaDexSharp/Internal/Dto/Responses/AtHome/AtHomeServerResponse.cs
@@ -1,10 +1,23 @@
 ﻿#nullable disable
 using System;
+using System.Collections.Generic;
 
 namespace MangaDexSharp.Internal.Dto.Responses.AtHome
 {
     internal class AtHomeServerResponse : MangaDexResponse
     {
         public string BaseUrl { get; set; }
+
+        /// <summary>
+        /// Contains Urls to full sized images of Chapter
+        /// </summary>
+        public IEnumerable<Uri> Data { get; set; }
+
+        /// <summary>
+        /// Contains Urls to compressed images of Chapter
+        /// </summary>
+        public IEnumerable<Uri> DataSaver { get; set; }
+
+        public string Hash { get; set; }
     }
 }
